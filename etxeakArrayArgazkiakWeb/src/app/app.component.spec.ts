8a7acee233a7383531d31c62d418e8ddbb6609b7
in2dm3-d <in2dm3-d@elorrieta.int>
2024-10-16 10:40:30 +0200

    izena aldatuta

--- etxeakArrayArgazkiakWeb/src/app/app.component.spec.ts
+++ etxeakArrayArgazkiakWeb/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Etxeak' title`, () => {
+  it(`should have the 'etxeakArrayArgazkiakWeb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Etxeak');
+    expect(app.title).toEqual('etxeakArrayArgazkiakWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Etxeak');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, etxeakArrayArgazkiakWeb');
   });
 });
